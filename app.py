d096e24913bf37b89a08b44febbe44eed0223dda
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-03-06 05:29:02 +0300

    edit menu and add bottom menu

--- app.py
+++ app.py
@@ -8,21 +8,31 @@ class WebApp():
 		self.collection_products = self.service_parser.get_products(parser.URL_DIRS["АКЦИИ"])
 		self.setup_routes()
 
+	def render_products(self, name_category:str) -> str:
+		return render_template(
+				'products.html',
+				array_products=self.service_parser.get_products(parser.URL_DIRS[name_category]),
+				)
+
 	def setup_routes(self):
 		@self.app.route('/products')
-		def index():
-			return render_template(
-				'products.html',
-				email="test@test.com",
-				Phone="8989898989898",
-				Location_office="office_test",
-				Copyright="2025",
-				IsLogin=True,
-				NameLogin="Admin",
-				IsAdmin=True,
-				array_products=self.collection_products,
-				)
-	
+		def route_products():
+			return self.render_products("АКЦИИ")
+
+		@self.app.route('/noutbuki')
+		def route_noutbuki():
+			return self.render_products("НОУТБУКИ")
+
+		@self.app.route('/monitori')
+		def route_monitori():
+			return self.render_products("МОНИТОРЫ")
+				
+		@self.app.route('/sistemniebloki')
+		def route_sistemniebloki():
+			return self.render_products("СИСТЕМНЫЕ БЛОКИ")
+
+
+
 	def start_app(self):
 		self.app.run(debug=False)
 
