650edbaef6da61bf14a4f59406cc7d072b0858d8
YuranIgnatenko <yuran.ignatenko@yandex.ru>
2025-03-06 09:31:29 +0300

    edit menu

--- app.py
+++ app.py
@@ -5,19 +5,25 @@ class WebApp():
 	def __init__(self) -> None:
 		self.app = Flask(__name__)
 		self.service_parser = parser.Parser()
-		self.collection_products = self.service_parser.get_products(parser.URL_DIRS["АКЦИИ"])
+		self.collection_products = self.service_parser.get_products(parser.URL_DIRS["АКЦИИ"]["АКЦИИ"])
 		self.setup_routes()
 
-	def render_products(self, name_category:str) -> str:
+	def render_products(self, name_category:str, key_category:str) -> str:
 		return render_template(
 				'products.html',
-				array_products=self.service_parser.get_products(parser.URL_DIRS[name_category]),
+				array_products = self.service_parser.get_products(parser.URL_DIRS[name_category][key_category]),
+				tab_1 = parser.URL_DIRS['ГАДЖЕТЫ'],
+				tab_2 = parser.URL_DIRS['ЭЛЕТРОНИКА'],
+				tab_3 = parser.URL_DIRS['ИНСТРУМЕНТЫ'],
+				tab_4 = parser.URL_DIRS['ФОТОАУДИОТЕХНИКА'],
+				tab_5 = parser.URL_DIRS['ПРОЧЕЕ'],				
+				tab_6 = parser.URL_DIRS['АКЦИИ'],
 				)
 
 	def setup_routes(self):
 		@self.app.route('/products')
 		def route_products():
-			return self.render_products("АКЦИИ")
+			return self.render_products("АКЦИИ","АКЦИИ")
 
 		@self.app.route('/noutbuki')
 		def route_noutbuki():
